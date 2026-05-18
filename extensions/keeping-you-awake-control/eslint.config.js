ec73fa4cf5ae81c656cdf185d82800cd6f13bfd6
Diego Schmidt <schmas+gh@pm.me>
2026-05-18 10:39:11 -0300

    chore(keeping-you-awake-control): bump deps to latest
    - @raycast/api ^1.104.17
    - @raycast/utils ^2.2.4 (major)
    - @raycast/eslint-config ^2.1.1
    - @types/node 22.19.17 (api peer)
    - prettier ^3.8.3
    
    eslint.config.js: ignore raycast-env.d.ts (auto-generated) and allow
    require in itself for compatibility with eslint-config 2.1.1 strict rules.

--- extensions/keeping-you-awake-control/eslint.config.js
+++ extensions/keeping-you-awake-control/eslint.config.js
@@ -2,5 +2,10 @@ const { defineConfig } = require("eslint/config");
 const raycastConfig = require("@raycast/eslint-config");
 
 module.exports = defineConfig([
+  { ignores: ["raycast-env.d.ts"] },
   ...raycastConfig,
+  {
+    files: ["eslint.config.js"],
+    rules: { "@typescript-eslint/no-require-imports": "off" },
+  },
 ]);
